d3c3746030325b67b4da0f111e08fb1f42627ed2
newprice247 <newprice247@gmail.com>
2023-11-15 12:41:13 -0500

    add photo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,78 +22,55 @@ function Navigation() {
   }, []);
  
   const navList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 text-white">
       <Link
         as="li"
         to="/"
         variant="small"
-        color="blue-gray"
+        color="white"
+        className={currentPage === '/' ? 'nav-link active p-1 font-normal' : 'nav-link p-1 font-normal'}
+      >
+          Home
+      </Link>
+      <Link
+        as="li"
+        to="/"
+        variant="small"
+        color="white"
         className={currentPage === '/' ? 'nav-link active p-1 font-normal' : 'nav-link p-1 font-normal'}
       >
           Projects
-      </Link>
-      <Typography
+      </Link><Link
         as="li"
+        to="/"
         variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
+        color="white"
+        className={currentPage === '/' ? 'nav-link active p-1 font-normal' : 'nav-link p-1 font-normal'}
       >
-        <a href="#" className="flex items-center">
           About
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="#" className="flex items-center">
-          Blocks
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="#" className="flex items-center">
-          Docs
-        </a>
-      </Typography>
+      </Link>
     </ul>
   );
  
   return (
-    <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
-      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+    <div className="-m-6 max-h-[768px] w-[calc(100%+48px)]">
+      <Navbar 
+      variant="gradient"
+      color="gray"
+      className="sticky  from-blue-300 to-gray-900 top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4" 
+      style={{ position: 'fixed' }}
+      >
         <div className="flex items-center justify-between text-blue-gray-900">
           <Typography
             as="a"
             href="#"
+            color="white"
             className="mr-4 cursor-pointer py-1.5 font-medium"
           >
             Nathaniel Price
           </Typography>
           <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
-            <div className="flex items-center gap-x-1">
-              <Button
-                variant="text"
-                size="sm"
-                className="hidden lg:inline-block"
-              >
-                <span>Log In</span>
-              </Button>
-              <Button
-                variant="gradient"
-                size="sm"
-                className="hidden lg:inline-block"
-              >
-                <span>Sign in</span>
-              </Button>
-            </div>
+            <div className="mr-20 hidden lg:block">{navList}</div>
             <IconButton
               variant="text"
               className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
@@ -133,27 +110,7 @@ function Navigation() {
             </IconButton>
           </div>
         </div>
-        <MobileNav open={openNav}>
-          {navList}
-          <div className="flex items-center gap-x-1">
-            <Button fullWidth variant="text" size="sm" className="">
-              <span>Log In</span>
-            </Button>
-            <Button fullWidth variant="gradient" size="sm" className="">
-              <span>Sign in</span>
-            </Button>
-          </div>
-        </MobileNav>
       </Navbar>
-      <div className="mx-auto max-w-screen-md py-12">
-        <Card className="mb-12 overflow-hidden">
-          <img
-            alt="nature"
-            className="h-[32rem] w-full object-cover object-center"
-            src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
-          />
-        </Card>
-      </div>
     </div>
   );
 }
