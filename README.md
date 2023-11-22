01fcec3f4f5ab8ec9be5b52ac0c5c73a552880a7
newprice247 <newprice247@gmail.com>
2023-11-21 23:36:56 -0500

    add readme

--- README.md
+++ README.md
@@ -1,5 +1,64 @@
 # React-Portfolio
 
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  ## Description
+
+  The goal of this project was to create a portfolio using my new React skills, one that would help set me apart from other developers.
+
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits) 
+  - [Contribution](#contribution)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  No installation necessary! [The app is hosted on netlify](https://nathanielportfolio.com/)
+  
+  If you would like to install to your local machine:
+  * Clone the repository to your local machine and navigate to the folder in your node terminal
+  * Run 'npm install' from the terminal to import the necessary node modules
+  * Enter 'npm start' inside the terminal to begin the application
+  * Navigate to 'localhost:3000' inside your browser to view the app
+
+  ## Usage
+
+  ![Nathaniel's Portfolio](<Screenshot 2023-11-21 232145.png>)
+
+  My React Portfolio is hosted on netlify
+  [Click here to check out the deployed site](https://nathanielportfolio.com/)
+
+
+  The portfolio is straightforward and easy to navigate, use the nav-bar at the top of the page to navigate to the different sections of the app. 
+  
+  * The header contains my name and the links to the various pages of the app. 
+  * The homepage features a welcome message and a type animation that displays various descriptions of the creator. 
+  * The Project page includes all the best projects I have worked on thus far in my coding journey as well as links to their respective Github repositories. 
+  * The About page includes a photo of myself, and a short bio. The Resume page includes a pdf representation of my resume, as well as buttons to download it if you're interested.
+  * The Contact page includes a form where you are able to enter your email address and a message and, using Formspree, it will be sent to my email account.
+  * The Contact page also includes links for various forms of correspondance. 
+  * The Footer contains a Copyright and links to more of my social media profiles.
+
+  ## Contribution
+
+  Send me feedback!
+
+  ## Credits
+
+  * UCF Full-Time Full-Stack Coding Bootcamp
+
+  ## License
+
+  [MIT](https://opensource.org/licenses/MIT)
+
+  ## Questions
+  
+  Reach out to me if you have any questions about the project!
+  
+  * Github:[ newprice247 @ Github](https://github.com/newprice247)
+
 
 Netlify Deployment:
 [https://calm-moonbeam-4ec4a8.netlify.app](https://calm-moonbeam-4ec4a8.netlify.app)
--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 // Import the `useForm` hook from the `@formspree/react` package in order to handle the form submission and send the data to Formspree, who will then send it to my email address
@@ -7,9 +7,37 @@ import { useForm, ValidationError } from '@formspree/react';
 export default function Contact() {
     // Formspree hook
     const [state, handleSubmit] = useForm("xoqozbdv");
+
+    // Handles form validation and sends an error message if the user tries to submit the form without filling out all the required fields
+    const [emailError, setEmailError] = useState(false);
+    const [subjectError, setSubjectError] = useState(false);
+    const [messageError, setMessageError] = useState(false);
+    const handleEmailBlur = (e) => {
+        if (e.target.value === "") {
+            setEmailError(true);
+        } else {
+            setEmailError(false);
+        }
+    };
+    const handleSubjectBlur = (e) => {
+        if (e.target.value === "") {
+            setSubjectError(true);
+        } else {
+            setSubjectError(false);
+        }
+    };
+    const handleMessageBlur = (e) => {
+        if (e.target.value === "") {
+            setMessageError(true);
+        } else {
+            setMessageError(false);
+        }
+    };
+
     if (state.succeeded) {
         return alert("Thank you for your message!");
     }
+
     return (
         <motion.div
             initial={{ opacity: 0 }}
@@ -17,37 +45,47 @@ export default function Contact() {
             viewport={{ once: true }}
             animate={{ y: -10 }}
             transition={{ delay: 0.2, duration: 0.7 }}
-            className="flex lg:flex-row flex-col justify-center items-center h-full">
+            className="flex lg:flex-row flex-col justify-center items-center h-full"
+        >
             <section className="bg-opacity-0 dark:bg-gray-900 ">
-                
                 <div className=" px-4 mx-auto max-w-screen-md mt-40 ">
                     <h2 class="mb-4 text-4xl  text-center text-myColor-3 ">Contact Me</h2>
-                    
-                    <p className="font-light text-center text-gray-200 sm:text-xl mb-10">Whether you want to get in touch, talk about a project collaboration, or just say hi, I'd love to hear from you.
-                        Simply fill out the form and send me an email!</p>
+                    <p className="font-light text-center text-gray-200 sm:text-xl mb-10">
+                        Whether you want to get in touch, talk about a project collaboration, or just say hi, I'd love to hear from you.
+                        Simply fill out the form and send me an email!
+                    </p>
                     <form
                         action="https://formspree.io/f/xoqozbdv"
                         className="space-y-8 mb-10 lg:mb-0"
                         method="POST"
-                        onSubmit={handleSubmit}>
+                        onSubmit={handleSubmit}
+                    >
                         <div>
-                            <label 
-                            htmlFor="email" 
-                            className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-300">Your email</label>
+                            <label
+                                htmlFor="email"
+                                className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-300"
+                            >
+                                Your email
+                            </label>
                             <input
                                 type="email"
                                 id="email"
                                 name="email"
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                className={`shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light ${
+                                    emailError ? "border-red-500" : ""
+                                }`}
                                 placeholder="name@flowbite.com"
                                 required
+                                onBlur={handleEmailBlur}
                             />
+                            {emailError && (
+                                <p className="text-red-500 text-sm">Email is required</p>
+                            )}
                             <ValidationError
                                 prefix="Email"
                                 field="email"
                                 errors={state.errors}
                             />
-
                         </div>
                         <div>
                             <label
@@ -60,10 +98,16 @@ export default function Contact() {
                                 type="text"
                                 id="subject"
                                 name="subject"
-                                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                className={`block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light ${
+                                    subjectError ? "border-red-500" : ""
+                                }`}
                                 placeholder="Let us know how we can help you"
                                 required
+                                onBlur={handleSubjectBlur}
                             />
+                            {subjectError && (
+                                <p className="text-red-500 text-sm">Subject is required</p>
+                            )}
                             <ValidationError
                                 prefix="Subject"
                                 field="subject"
@@ -71,16 +115,25 @@ export default function Contact() {
                             />
                         </div>
                         <div className="sm:col-span-2">
-                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-400">Your message</label>
+                            <label
+                                htmlFor="message"
+                                className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-400"
+                            >
+                                Your message
+                            </label>
                             <textarea
                                 id="message"
                                 rows="6"
                                 name="message"
-                                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                className={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 ${
+                                    messageError ? "border-red-500" : ""
+                                }`}
                                 placeholder="Leave a comment..."
-
-                            >
-                            </textarea>
+                                onBlur={handleMessageBlur}
+                            ></textarea>
+                            {messageError && (
+                                <p className="text-red-500 text-sm">Message is required</p>
+                            )}
                             <ValidationError
                                 prefix="Message"
                                 field="message"
@@ -91,7 +144,9 @@ export default function Contact() {
                             type="submit"
                             className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-400 sm:w-fit hover:bg-myColor-3 hover:text-black focus:ring-4 focus:outline-none focus:ring-primary-300"
                             disabled={state.submitting}
-                        >Send message</button>
+                        >
+                            Send message
+                        </button>
                     </form>
                 </div>
             </section>
@@ -117,4 +172,3 @@ export default function Contact() {
         </motion.div>
     );
 }
-
