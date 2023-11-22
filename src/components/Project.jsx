62013427a79a267f3ee936643ecc7a9d21f313a6
newprice247 <newprice247@gmail.com>
2023-11-22 11:17:19 -0500

    add links to deployed site or video

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -12,31 +12,47 @@ import {
 // Exports the Project component, which is used to display the projects on the ProjectPage by mapping over the projects array located in ProjectPage.jsx
 export default function Project(props) {
     return (
-    <Card className="mt-20 w-96  bg-myColor-1 " style={{ backgroundImage: 'url("/shutterstock_228650476.jpg") ' }}>
-                <CardHeader color="white" className="relative">
-                    <img
-                        src={props.projectImage}
-                        alt="card-image"
-                    />
-                </CardHeader>
-                <CardBody 
-                    className="text-myColor-3"
-                    
-                >
-                    <Typography variant="h5" color="blue-gray" className="mb-2 text-white">
-                        {props.projectName}
-                    </Typography>
-                    <Typography>
-                        {props.projectDescription}
-                    </Typography>
-                </CardBody>
-                <CardFooter className="pt-0">
-                    <Button 
+        <Card className="mt-20 w-96  bg-myColor-1 " style={{ backgroundImage: 'url("/shutterstock_228650476.jpg") ' }}>
+            <CardHeader color="white" className="relative">
+                <img
+                    src={props.projectImage}
+                    alt="card-image"
+                />
+            </CardHeader>
+            <CardBody
+                className="text-myColor-3"
+
+            >
+                <Typography variant="h5" color="blue-gray" className="mb-2 text-white">
+                    {props.projectName}
+                </Typography>
+                <Typography>
+                    {props.projectDescription}
+                </Typography>
+            </CardBody>
+            <CardFooter className="pt-0">
+                <Button
                     className="hover:bg-blue-400 hover:text-myColor-1 transition-transform hover:scale-105"
                     onClick={() => window.open(props.projectLink, "_blank")}>
-                        Github Repo
+                    Github Repo
+                </Button>
+                {props.projectLink2 && (
+                    <Button
+                        className="hover:bg-blue-400 hover:text-myColor-1 transition-transform hover:scale-105 ml-5"
+                        onClick={() => window.open(props.projectLink2, "_blank")}
+                    >
+                        Deployed App
                     </Button>
-                </CardFooter>
-            </Card>
+                )}
+                {props.projectLink3 && (
+                    <Button
+                        className="hover:bg-blue-400 hover:text-myColor-1 transition-transform hover:scale-105 ml-5"
+                        onClick={() => window.open(props.projectLink3, "_blank")}
+                    >
+                        Walkthrough Video
+                    </Button>
+                )}
+            </CardFooter>
+        </Card>
     );
 }
--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -11,60 +11,70 @@ const projects = [
         projectDescription: "The goal of this project was to build a text editor that runs in the browser. The app is a single-page application that features a number of data persistance techniques that serve as redundancyies in the case one of the data storing options is not supported by the browser, and can function offline.",
         projectImage: "/texteditor1.png",
         projectLink: "https://github.com/newprice247/PWA-Text_Editor",
+        projectLink2: "https://np-pwa-text-editor-fa826b6e9b97.herokuapp.com/",
     },
     {
         projectName: "F2F Blog",
         projectDescription: "Presenting: F2F Blog! Our blog offers educational content, tutorials, and resources that cater to coders of all levels. Whether you're just starting or seeking to master a specific programming language, we've got you covered.",
         projectImage: "/f2fblog.png",
         projectLink: "https://github.com/newprice247/F2F-Blog",
+        projectLink2: "https://f2f-blog-ad8f414d0b59.herokuapp.com/",
     },
     {
         projectName: "Social Network API",
         projectDescription: "The goal of this project was to build an API for a social network web application that uses Express.js for routing, and a MongoDB database that stores the data by utilizing the Mongoose ODM library. The app will offer a place where users can share their thoughts, react to freinds' thoughts, and create a friend list.",
         projectImage: "/socialNetwork.png",
         projectLink: "https://github.com/newprice247/Social-Network-API",
+        projectLink3: "https://drive.google.com/file/d/1EQ9JkRFMkT_i2WfHXY2itFFuET1eLpMo/view",
     },
     {
         projectName: "Employee Tracker",
         projectDescription: "Employee Tracker is a command-line application that uses Node.js, Inquirer, and MySQL to manage a company's employee database.",
         projectImage: "/employeeTracker.gif",
         projectLink: "https://github.com/newprice247/Employee-Tracker",
+        projectLink3: "https://drive.google.com/file/d/1TrxnsZv5zZhxFxpnwEMd-gijA4BfQGOi/view",
     },
     {
         projectName: "E Commerce Back End",
         projectDescription: "A Node.js application that uses Express.js API and Sequelize to interact with a MySQL database. The app allows the user to create, read, update, and delete data from the database using GET, POST, PUT, and DELETE routes.",
         projectImage: "/eCommerce.png",
         projectLink: "https://github.com/newprice247/E-Commerce-Back-End",
+        projectLink3: "https://drive.google.com/file/d/1STiGIJiF43sKlfpDXGPgdc_MC4mh00Tn/view",
     },
     {
         projectName: "Note Taker",
         projectDescription: "Note-Taker is a user-friendly note-taking app that simplifies the process of jotting down, organizing, and managing your thoughts, ideas, and tasks. With Note-Taker, you can effortlessly create, edit, save, and delete notes, ensuring that you have a digital workspace that adapts to your needs.",
         projectImage: "/noteTaker.gif",
         projectLink: "https://github.com/newprice247/Note-Taker",
+        projectLink2: "https://note-taker-newprice247-e6c2f2c3bf4d.herokuapp.com/notes",
     },
     {
         projectName: "Weather Dashboard",
         projectDescription: "The Weather Dashboard is a simple weather app that allows the user to search for a city and view the current weather conditions as well as the five-day forecast. The app uses the OpenWeather API to retrieve weather data for cities.",
         projectImage: "/weatherDashboard.png",
-        projectLink: "https://github.com/newprice247/Weather-Dashboard"
+        projectLink: "https://github.com/newprice247/Weather-Dashboard",
+        projectLink2: "https://newprice247.github.io/Weather-Dashboard/",
     },
     {
         projectName: "Work Day Scheduler",
         projectDescription: "The Work Day Scheduler is a simple calendar application that allows the user to save events for each hour of the day. The app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.",
         projectImage: "/workDayScheduler.png",
-        projectLink: "https://github.com/newprice247/Work-Day-Scheduler"
+        projectLink: "https://github.com/newprice247/Work-Day-Scheduler",
+        projectLink2: "https://newprice247.github.io/Work-Day-Scheduler/",
     },
     {
         projectName: "Bust A Movie",
         projectDescription: "Bust A Movie is a movie search engine that allows users to search for movies by title. The app uses the Open Movie Database API to retrieve movie data for the user's search query",
         projectImage: "/bustAMovie.png",
-        projectLink: "https://github.com/newprice247/Bust-A-Movie"
+        projectLink: "https://github.com/newprice247/Bust-A-Movie",
+        projectLink2: "https://newprice247.github.io/Bust-A-Movie/",
     },
     {
         projectName: "Code Quiz",
         projectDescription: "This Code Quiz app is a timed quiz on JavaScript fundamentals that stores high scores so that the user can gauge their progress compared to their peers",
         projectImage: "/codeQuiz.png",
-        projectLink: "https://github.com/newprice247/Code-Quiz-Mod-4"
+        projectLink: "https://github.com/newprice247/Code-Quiz-Mod-4",
+        projectLink2: "https://newprice247.github.io/Code-Quiz-Mod-4/",
     },
 ];
 
@@ -107,6 +117,8 @@ export default function ProjectPage() {
                     projectDescription={project.projectDescription}
                     projectImage={project.projectImage}
                     projectLink={project.projectLink}
+                    projectLink2={project.projectLink2}
+                    projectLink3={project.projectLink3}
                 />
             ))}
         </motion.div>
